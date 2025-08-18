75722249658e224a5ac34962a10c649957e41791
Maël GUERIN <mael.guerin@murena.io>
2025-08-18 09:51:48 +0200

    dev: enable virtualization

--- dev/default.nix
+++ dev/default.nix
@@ -1,7 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  virtualisation.libvirtd.enable = true;
   virtualisation.docker.enable = true;
+
+  programs.virt-manager.enable = true;
 
   environment.systemPackages = with pkgs; [
     vscode

af91a329d48534694bba1655c89b375f08ab3743
Maël GUERIN <mael.guerin@murena.io>
2025-08-18 09:52:01 +0200

    desktops: enable flatpak

--- desktops/common/default.nix
+++ desktops/common/default.nix
@@ -61,6 +61,8 @@ in
     networking.firewall.allowedTCPPorts = [ 8611 ];
     networking.firewall.allowedUDPPorts = [ 8611 ];
 
+    services.flatpak.enable = true;
+
     # Enable sound with pipewire.
     services.pulseaudio.enable = false;
     security.rtkit.enable = true;
